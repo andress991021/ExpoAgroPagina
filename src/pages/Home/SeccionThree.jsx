0a177ab08a717272032538b534750c91113e732a
andress991021 <andress991021@gmail.com>
2024-08-01 02:06:18 -0500

    cambios home fase 2

--- src/pages/Home/SeccionThree.jsx
+++ src/pages/Home/SeccionThree.jsx
@@ -9,9 +9,80 @@ import icon2 from "../../assets/SectionThree/icono2.jpg";
 import ondaFade from "../../assets/SectionTwo/OndaFade.svg";
 import ondaBG from "../../assets/SectionTwo/bgOnda.svg";
 
+import Banner01 from "../../assets/SectionThree/Banner01.png";
+import Imagen01 from "../../assets/SectionThree/Imagen01.png";
+import Imagen02 from "../../assets/SectionThree/Imagen02.png";
+import Imagen03 from "../../assets/SectionThree/Imagen03.png";
+import Cenefa01 from "../../assets/SectionThree/Cenefa01.svg";
+import Cenefa02 from "../../assets/SectionThree/Cenefa02.svg";
+import Cenefa03 from "../../assets/SectionThree/Cenefa03.svg";
+import banner from "../../assets/Quienes_Somos/Banner.png";
+
 export const SeccionThree = () => {
     return (
+        <div>
+      <div className="  w-full ">
+        <img className="w-full lg:w-full" src={Banner01} alt="" />
+      </div>
 
+      <div className="grid grid-cols-1 lg:grid-cols-1 static z-40">
+        <center>
+          <img
+            className="w-full hidden justify-center items-center lg:block lg:w-full lg:-mt-8 "
+            src=""
+            alt=""
+          />
+        </center>
+          <div className="mx-20  lg:w-3/4">
+            <h1 className="text-xl m-10 text-green-800 sm:text-7xl  font-bold  text-center md:text-left ">
+              Quienes Somos
+            </h1>
+
+            <br />
+            <p className="text-base lg:text-3xl  m-10 -mt-10 lg:w-full text-justify">
+              Somos una empresa consolidada en el sector forestal y agrícola,
+              destacándonos por nuestro compromiso, nos especializamos en la
+              recolección, producción e importación de semillas, garantizando
+              una calidad superior y una amplia diversidad para satis- facer las
+              necesidades de nuestros clientes.
+            </p>
+
+            <h2 className="text-2xl m-10 text-green-800 sm:text-7xl  font-bold  text-left ">
+              Visión
+            </h2>
+            <br />
+            <p className="text-base lg:text-3xl   m-10 -mt-10 lg:w-full text-justify">
+              Mantenernos como una empresa líder y competitiva en el sector
+              agroforestal, destacada por nuestra calidad y compromiso, es nues-
+              tro objetivo principal.
+            </p>
+            <h2 className="text-2xl m-10 -mt-4 text-green-800 sm:text-7xl sm:mt-4  font-bold  text-left ">
+              Misión
+            </h2>
+            <br />
+            <p className="text-base lg:text-3xl   m-10 -mt-10 lg:w-full text-justify">
+              Semillas Expoagro SAS es una empresa líder en el sector
+              agroforestal, reconocida por sus rigurosos estándares de calidad y
+              su competitivi- dad. Nuestro compromiso con el crecimiento del
+              sector productivo del campo y la conservación del medio ambiente
+              es primordial en todos nuestros procedimientos.
+            </p>
+          </div>
+        </div>
+
+        <div className="  w-full ">
+        <img className="w-full lg:w-full" src={Cenefa01} alt="" />
+      </div>
+      
+      <div className="  w-full ">
+        <img className="w-full lg:w-full" src={Cenefa02} alt="" />
+      </div>
+
+      <div className="  w-full ">
+        <img className="w-full lg:w-full" src={Cenefa03} alt="" />
+      </div>
+
+        
         <div>
             <div className="hidden lg:block">
                 <video preload="true" autoPlay loop muted>
@@ -27,90 +98,36 @@ export const SeccionThree = () => {
             <div className="grid grid-cols-1 lg:grid-cols-2 static z-40">
                 <div>
                     <center>
-                        <img className="w-full sm:w-3/4 lg:w-3/4" src={imgThree} alt="" />
+                        <img className="w-full sm:w-3/4 lg:w-3/4" src={Imagen03} alt="" />
                     </center>
 
                 </div>
                 <div className="mx-auto sm:-mt-20  lg:w-3/4 lg:mt-5">
                     <h1 className="text-3xl -mt-10 sm:mt-10 m-10 text-brown-900 sm:text-5xl font-bold  text-left ">
-                        Experiencia
-                        garantizada
+                    Estamos certificados
                     </h1>
                     <p className="text-base lg:text-lg m-10 lg:w-full text-justify">
-                        Gracias a la confianza de nuestros clientes, hemos podido producir más de 100.000 árboles anuales de
-                        nuestras diversas especies como las semillas forestales, frutales, agrícolas y hortalizas siendo distribuidas
-                        con nuestros principales aliados comerciales alrededor del país y algunos países extranjeros.
-                        Cada uno de estos alcances, nos han fortalecido como el principal recolector directo de semillas
-                        en Colombia con los mejores productos de alta calidad. En la actualidad contamos con los registros como:
+                    Semillas ExpoAgro sas. es una empresa certificada por el Instituto Colombiano Agropecuario ICA para comercializar, empacar e importar semillas de alta calidad con cobertura nacional e internacional.
                     </p>
-
+                    <h1 className="text-3xl -mt-10 sm:mt-10 m-10 text-brown-900 sm:text-5xl font-bold  text-left ">
+                    Contamos con Registro como:
+                    </h1>
+                    
                     <ul className="font-bold block mt-10 m-10">
-                        <li>• Distribuidor</li>
-                        <li>• Exportador</li>
-                        <li>• Importador</li>
-                        <li>• Productor</li>
-                        <li>• Reempaque</li>
+                        <li>Registro de Importador N° 00026693</li>
+                        <li>Registro de Comercializador N° CM0056662022</li>
+                        <li>Registro de Reempaque N° 00000705</li>
+                        <li>Registro de Productor de semilla seleccionada N° 00006131</li>
                     </ul>
                 </div>
             </div>
 
             {/*Fondo Onda fadeada */}
-            <div className=" hidden w-full relative -z-20 lg:block lg:mt-10">
-                <img className="w-full lg:w-full absolute -top-[32rem] lg:-top-[32rem]" src={ondaBG} alt="" />
-            </div>
-
-            <div className="block w-full lg:hidden  -z-20 ">
-                <img className="w-full lg:w-full " src={ondaBG} alt="" />
-            </div>
-
-            <div className="grid grid-cols-1 lg:grid-cols-2 static z-40 lg:m-32">
-
-                <div className="mx-auto sm:-mt-16 lg:w-3/4">
-                    <h1 className="text-3xl m-10 text-brown-900 sm:text-5xl font-bold  text-left lg:mt-32 ">
-                        Respaldo
-                    </h1>
-                    <p className="text-base lg:text-lg m-10 lg:w-full text-justify">
-                        Mantenemos estrechos lazos con el mercado de semillas en latinoamerica,
-                        en momentos que en los distintos países firman acuerdos de libre comercio
-                        y el sector agrícola se expande y se une para ser competitivo en el mercado
-                        internacional.
-                    </p>
-
-                    <center>
-                        <img className="block w-full -mt-20 sm:w-3/4 sm:-mt-20 lg:w-3/4 lg:hidden" src={imgThree2} alt="" />
-                    </center>
-
-                    <div className="m-10">
-                        <img className="w-44  lg:w-32" src={icon1} alt="" />
-                        <div className="mt-5">
-                            <p className="text-base lg:text-lg font-bold  lg:w-full text-justify">COSTA RICA</p>
-                            <p className="text-base lg:text-lg  lg:w-full text-justify">
-                                Distribuidores autorizados para Colombia en semilla de Teca Escarificada Mejorada
-                                y Melina de Huerto Semillero de Semillas y Bosques.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="m-10">
-                        <img className="W-52 lg:w-32" src={icon2} alt="" />
-                        <div className="mt-5">
-                            <p className="text-base lg:text-lg font-bold lg:w-full text-justify">HONDURAS</p>
-                            <p className="text- base lg:text-lg lg:w-full text-justify">
-                                Distribuidores autorizados para Colombia en semilla de Pino
-                                Caribea de Semillas Setro.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    <center>
-                        <img className="hidden lg:block w-full lg:w-3/4" src={imgThree2} alt="" />
-                    </center>
-                </div>
-            </div>
+      
+       
 
         </div>
-
+        </div>
 
     )
 
--- src/pages/Home/SeccionTwo.jsx
+++ src/pages/Home/SeccionTwo.jsx
@@ -1,15 +1,5 @@
 import React from "react";
-import imgQuality from "../../assets/SectionTwo/QualityImage.png";
-import imgQuality2 from "../../assets/SectionTwo/QualityImage2.png";
-import icon1 from "../../assets/SectionTwo/img01.svg";
-import icon2 from "../../assets/SectionTwo/img02.svg";
-import icon3 from "../../assets/SectionTwo/img03.svg";
-import icon4 from "../../assets/SectionTwo/img04.svg";
-import ondaBG from "../../assets/SectionTwo/bgOnda.svg";
-import ondaFade from "../../assets/SectionTwo/OndaFade.svg";
-import TimelineWeb from "../../assets/SectionTwo/TimelineWeb.svg";
-import TimelineMobile from "../../assets/SectionTwo/TimelineMobile.svg";
-import CenefaOne from "../../assets/SectionTwo/bgOnda.svg";
+
 
 import Cenefa01 from "../../assets/SectionTwo/Cenefa01.svg";
 import Icon01 from "../../assets/SectionTwo/Icon01.svg";
@@ -20,6 +10,8 @@ import Banner01 from "../../assets/SectionTwo/Banner01.jpg";
 import Banner02 from "../../assets/SectionTwo/Banner02.jpg";
 import Banner03 from "../../assets/SectionTwo/Banner03.jpg";
 import Banner04 from "../../assets/SectionTwo/Banner04.jpg";
+import Banner05 from "../../assets/SectionTwo/Banner05.jpg";
+import Banner06 from "../../assets/SectionTwo/Banner06.jpg";
 import BottonOn from "../../assets/SectionTwo/BottonOn.svg";
 import BottonOff from "../../assets/SectionTwo/BottonOff.svg";
 
@@ -88,16 +80,15 @@ export const SeccionTwo = () => {
             alt="Button On"
           />
         </div>
-        
 
         <div className="w-full relative opacity-40 hover:opacity-100  group ">
-          <img className=" mx-auto" src={Banner01} alt="" />
+          <img className=" mx-auto" src={Banner02} alt="" />
           <div className="absolute inset-x-4 top-0 m-10 ">
             <h1 className="text-3xl text-white sm:text-4xl  font-light  text-left  ">
               Semillas
             </h1>
             <h1 className="text-3xl text-white sm:text-5xl  font-medium  text-left  ">
-            Hortalizas
+              Hortalizas
             </h1>
           </div>
 
@@ -113,15 +104,14 @@ export const SeccionTwo = () => {
           />
         </div>
 
-
         <div className="w-full relative opacity-40 hover:opacity-100  group ">
-          <img className=" mx-auto" src={Banner01} alt="" />
+          <img className=" mx-auto" src={Banner03} alt="" />
           <div className="absolute inset-x-4 top-0 m-10 ">
             <h1 className="text-3xl text-white sm:text-4xl  font-light  text-left  ">
               Semillas
             </h1>
             <h1 className="text-3xl text-white sm:text-5xl  font-medium  text-left  ">
-            Agrícolas
+              Agrícolas
             </h1>
           </div>
 
@@ -137,9 +127,8 @@ export const SeccionTwo = () => {
           />
         </div>
 
-
         <div className="w-full relative opacity-40 hover:opacity-100  group ">
-          <img className=" mx-auto" src={Banner01} alt="" />
+          <img className=" mx-auto" src={Banner04} alt="" />
           <div className="absolute inset-x-4 top-0 m-10 ">
             <h1 className="text-3xl text-white sm:text-4xl  font-light  text-left  ">
               Semillas
@@ -161,9 +150,8 @@ export const SeccionTwo = () => {
           />
         </div>
 
-
         <div className="w-full relative opacity-40 hover:opacity-100  group ">
-          <img className=" mx-auto" src={Banner01} alt="" />
+          <img className=" mx-auto" src={Banner05} alt="" />
           <div className="absolute inset-x-4 top-0 m-10 ">
             <h1 className="text-3xl text-white sm:text-4xl  font-light  text-left  ">
               Semillas
@@ -185,9 +173,8 @@ export const SeccionTwo = () => {
           />
         </div>
 
-
         <div className="w-full relative opacity-40 hover:opacity-100  group ">
-          <img className=" mx-auto" src={Banner01} alt="" />
+          <img className=" mx-auto" src={Banner06} alt="" />
           <div className="absolute inset-x-4 top-0 m-10 ">
             <h1 className="text-3xl text-white sm:text-4xl  font-light  text-left  ">
               Semillas
@@ -207,119 +194,6 @@ export const SeccionTwo = () => {
             src={BottonOn}
             alt="Button On"
           />
-        </div>
-
-
-      </div>
-
-      <div className="mt-8 font-Roboto">
-        <div>
-          <div className="grid grid-cols-1 lg:grid-cols-2 static z-40">
-            <div>
-              <center>
-                <img
-                  className="w-full sm:w-3/4 lg:w-3/4"
-                  src={imgQuality}
-                  alt=""
-                />
-              </center>
-            </div>
-            <div className="mx-auto  lg:w-3/4">
-              <h1 className="text-3xl m-10 text-brown-900 sm:text-5xl  font-bold  text-left ">
-                La calidad de la vida está en la calidad de la semilla.
-              </h1>
-              <p className="text-base lg:text-lg m-10 lg:w-full text-justify">
-                Semillas RBC trae para ti productos de la más alta calidad
-                cumpliendo con los estándares establecidos y de esta forma
-                garantizando un servicio óptimo, para que tus productos sean
-                entregados en los tiempos dispuestos y así podamos ser parte de
-                tus metas y proyectos a lograr. Por eso hemos dispuesto para ti
-                el siguiente portafolio de productos.
-              </p>
-            </div>
-          </div>
-          {/*Fondo Onda fadeada */}
-          <div className=" hidden w-full relative -z-20 lg:block">
-            <img
-              className="w-full lg:w-full absolute -top-[32rem] lg:-top-[32rem]"
-              src={ondaFade}
-              alt=""
-            />
-          </div>
-        </div>
-
-        <div className="block w-full lg:hidden  -z-20 ">
-          <img className="w-full lg:w-full " src={ondaFade} alt="" />
-        </div>
-
-        <div className="grid grid-cols-2 -mt-20 sm:grid-cols-2 lg:grid-cols-4 lg:mt-48">
-          <div className="mx-auto mt-12">
-            <img className="w-52 lg:w-80" src={icon1} alt="" />
-          </div>
-
-          <div className="mx-auto mt-12">
-            <img className="w-52 lg:w-80" src={icon2} alt="" />
-          </div>
-
-          <div className="mx-auto mt-12">
-            <img className="w-52 lg:w-80" src={icon3} alt="" />
-          </div>
-
-          <div className="mx-auto mt-12">
-            <img className="w-52 lg:w-80" src={icon4} alt="" />
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 lg:grid-cols-2 static z-40">
-          <div className="mx-auto lg:w-3/4 sm:m-10 ">
-            <h1 className="text-3xl m-10 text-brown-900 sm:text-5xl font-bold   text-left ">
-              Quienes somos
-            </h1>
-            <p className="text-base lg:text-lg m-10 lg:w-full text-justify">
-              Semillas RBC S.A.S con 30 años de experiencia en la recolección y
-              selección de semillas nativas para la reforestación ambiental,
-              comercial en Maderables y Frutales, desarrollamos el manejo de
-              nuevas alternativas que hagan más fácil y efectiva la adquisición
-              de productos de excelente calidad.
-            </p>
-          </div>
-
-          <div className="justify-center items-center">
-            <center id="seccionnuestrahistoriamobile">
-              <img
-                className="w-full -mt-16 justify-center items-center sm:-mt-28 sm:w-3/4 lg:w-3/4 lg:mt-16 "
-                src={imgQuality2}
-                alt=""
-              />
-            </center>
-          </div>
-        </div>
-
-        <div id="seccionnuestrahistoria">
-          <h1 className="text-3xl text-center mb-5 lg:m-10 text-brown-900 sm:text-5xl font-bold   lg:text-left ">
-            Nuestra Historia
-          </h1>
-          {/* Timeline Web */}
-          <div className="grid grid-cols-1 static bg-brown-900">
-            <center>
-              <img
-                className="w-full hidden justify-center items-center lg:block lg:w-full "
-                src={TimelineWeb}
-                alt=""
-              />
-            </center>
-          </div>
-
-          {/* Timeline Mobile */}
-          <div className="grid grid-cols-1 static bg-brown-900">
-            <center>
-              <img
-                className="w-full block lg:hidden "
-                src={TimelineMobile}
-                alt=""
-              />
-            </center>
-          </div>
         </div>
       </div>
     </div>
